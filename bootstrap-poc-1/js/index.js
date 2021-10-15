e71515dafc18f8d4e210c863214a0fdb3baa687f
Kancherla <saivittal.kancherla@acsicorp.com>
2021-10-15 10:25:01 +0530

    fully created first version

--- bootstrap-poc-1/js/index.js
+++ bootstrap-poc-1/js/index.js
@@ -4,56 +4,56 @@ const imagePath = "images/";
 const tabsId = document.getElementById("tabs");
 const tabId = document.getElementById("tabId");
 const tabClass = document.getElementsByClassName("tab");
-tabClass[0].style.borderTop = "4px solid #1bbc9b" ;
-tabClass[0].style.borderBottomColor = "white" ;
+let prevActiveCategory = featured_works_categories.firstElementChild;
+let prevActiveTab = tabClass[0];
 const featured_works_images_array = [{
-    "imageName": "website-1.jfif",
-    "category": "website"
-},
-{
-    "imageName": "website-2.png",
-    "category": "website"
-},
-{
-    "imageName": "website-3.jpg",
-    "category": "website"
-},
-{
-    "imageName": "logos-1.png",
-    "category": "logos"
-},
-{
-    "imageName": "logos-2.png",
-    "category": "logos"
-},
-{
-    "imageName": "logos-3.png",
-    "category": "logos"
-},
-{
-    "imageName": "gd-1.jfif",
-    "category": "graphic"
-},
-{
-    "imageName": "gd-2.jpg",
-    "category": "graphic"
-},
-{
-    "imageName": "om-1.jpg",
-    "category": "online"
-},
-{
-    "imageName": "om-2.jfif",
-    "category": "online"
-},
-{
-    "imageName": "v-1.jfif",
-    "category": "video"
-},
-{
-    "imageName": "v-2.png",
-    "category": "video"
-}
+        "imageName": "website-1.jfif",
+        "category": "website"
+    },
+    {
+        "imageName": "website-2.png",
+        "category": "website"
+    },
+    {
+        "imageName": "website-3.jpg",
+        "category": "website"
+    },
+    {
+        "imageName": "logos-1.png",
+        "category": "logos"
+    },
+    {
+        "imageName": "logos-2.png",
+        "category": "logos"
+    },
+    {
+        "imageName": "logos-3.png",
+        "category": "logos"
+    },
+    {
+        "imageName": "gd-1.jfif",
+        "category": "graphic"
+    },
+    {
+        "imageName": "gd-2.jpg",
+        "category": "graphic"
+    },
+    {
+        "imageName": "om-1.jpg",
+        "category": "online"
+    },
+    {
+        "imageName": "om-2.jfif",
+        "category": "online"
+    },
+    {
+        "imageName": "v-1.jfif",
+        "category": "video"
+    },
+    {
+        "imageName": "v-2.png",
+        "category": "video"
+    }
 ];
 
 function load_featured_works_images(category) {
@@ -77,31 +77,41 @@ function load_featured_works_images(category) {
 }
 load_featured_works_images("all");
 
-featured_works_categories.onclick = function(e){
-    if(e.target.tagName === "BUTTON"){
+featured_works_categories.onclick = function (e) {
+    if (e.target.tagName === "BUTTON") {
+        prevActiveCategory.classList.remove("active");
+        e.target.classList.add("active");
+        prevActiveCategory = e.target;
         load_featured_works_images(e.target.dataset.name);
     }
 }
 
 tabsId.onclick = (e) => {
-    let target = e.target, dataId, documentObject;
-    if(target.tagName === "DIV"){
-        documentObject = target ;
-         dataId = target.dataset.id;
+    let target = e.target,
+        dataId, documentObject;
+    if (target.tagName === "DIV") {
+        documentObject = target;
+        dataId = target.dataset.id;
     }
-    if(target.tagName === "STRONG"){
-        documentObject = target.parentNode ;
+    if (target.tagName === "STRONG") {
+        documentObject = target.parentNode;
         dataId = target.parentNode.dataset.id;
     }
-    tabId.innerHTML = dataId;
-    for(let i = 0 ; i < tabClass.length ; i++ ){
-        if(tabClass[i].dataset.id !== dataId){
-            tabClass[i].style.borderBottomColor = "#abb3b4" ;
-            tabClass[i].style.borderTop = "" ;
-        }
-        else{
-            tabClass[i].style.borderTop = "4px solid #1bbc9b" ;
-            tabClass[i].style.borderBottomColor = "white" ;
-        }
+    if (dataId) {
+        tabId.innerHTML = dataId;
+        prevActiveTab.classList.remove("active");
+        documentObject.classList.add("active");
+        prevActiveTab = documentObject;
     }
+
+    // for(let i = 0 ; i < tabClass.length ; i++ ){
+    //     if(tabClass[i].dataset.id !== dataId){
+    //         tabClass[i].style.borderBottomColor = "#abb3b4" ;
+    //         tabClass[i].style.borderTop = "" ;
+    //     }
+    //     else{
+    //         tabClass[i].style.borderTop = "4px solid #1bbc9b" ;
+    //         tabClass[i].style.borderBottomColor = "white" ;
+    //     }
+    // }
 }
--- bootstrap-poc-1/js/slick.js
+++ bootstrap-poc-1/js/slick.js
@@ -7,3 +7,37 @@ $('.image_slider').slick({
     draggable: true,
     dots:true,
   });
+  $(".photography_images").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    draggable: true,
+    //dots:true,
+  })
+
+  $(".sub_images").slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: true,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: true,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+  })
