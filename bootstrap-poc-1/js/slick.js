7a8515b0831a0427e340ea0f3f35ebc6eff88d74
Kancherla <saivittal.kancherla@acsicorp.com>
2021-10-19 16:56:04 +0530

    final version with some modifications done git add .

--- bootstrap-poc-1/js/slick.js
+++ bootstrap-poc-1/js/slick.js
@@ -1,43 +1,42 @@
 //$('.main_image').slick();
 $('.image_slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    draggable: true,
-    dots:true,
-  });
-  $(".photography_images").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    draggable: true,
-    //dots:true,
-  })
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  draggable: true,
+  dots: true,
+});
+$(".twitter_posts").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  draggable: true,
+})
 
-  $(".sub_images").slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    // responsive: [
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       arrows: true,
-    //       centerMode: true,
-    //       centerPadding: '40px',
-    //       slidesToShow: 3
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       arrows: true,
-    //       centerMode: true,
-    //       centerPadding: '40px',
-    //       slidesToShow: 1
-    //     }
-    //   }
-    // ]
-  })
+$(".sub_images").slick({
+  centerMode: true,
+  centerPadding: '100px',
+  slidesToShow: 1,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: true,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: true,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+})
